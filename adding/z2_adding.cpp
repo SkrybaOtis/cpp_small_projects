85b8bac6cc0db8295a38dd484b987acd03a52620
otis <ul0265624@edu.uni.lodz.pl>
2025-08-06 21:40:16 +0200

    Add tasks in dirs

---
+++ adding/z2_adding.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+unsigned long long reverse(unsigned long long q){
+	unsigned long long v = 0;
+	while(q>0){
+		v = v + (q % 10);
+		v *=10;
+		q = q / 10;
+	}
+
+	return v/10;
+}
+
+int main(){
+	unsigned long long n, x, y;
+	
+	std::cin >> n;
+	for(unsigned long long i=0; i<n; i++){
+		std::cin >> x >> y;
+		std::cout << reverse(reverse(x) + reverse(y)) << std::endl;
+	}
+	
+	return 0;
+}
---
+++ helix/z1_helix.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+
+int bigger(int a, int b){
+	if(a>b) { return a; } else { return b; }
+}
+
+int main() {
+
+	int largest, sum1, sum2;
+    	int n1, n2;
+    
+    while (std::cin >> n1 && n1 != 0) { 	//sprawdzenie, czy to koniec przypadków testowych
+    
+        std::vector<int> v1(n1); 		//wczytanie długości 1 ciągu
+        for (int i = 0; i < n1; i++) {
+            std::cin >> v1[i];			//wczytanie elementów 1 ciągu
+        } 
+        
+        std::cin >> n2; 			//wczytanie długości 2 ciągu
+        std::vector<int> v2(n2);
+        for (int i = 0; i < n2; i++) {
+            std::cin >> v2[i];			//wczytanie elementów 2 ciągu
+        }
+        
+        std::vector<int> intersectionPoints;	//stworzenie wektora elementów wspólnych ciągów
+        int i = 0, j = 0;
+        
+        
+        while (i < n1 && j < n2) {	//pętla wczytująca cyfry wspólne do wektora elementów wspólnych ciągu
+            if (v1[i] < v2[j]) { i++; }
+            else if (v1[i] > v2[j]) { j++; }
+            else { intersectionPoints.push_back(v1[i]); i++; j++; }
+        }
+        
+        sum1 = 0; sum2 = 0; largest = 0; 	//largest jest zmienną z największą możliwą sumą liczb ciągów
+        i = j = 0; 
+        
+        for (int k : intersectionPoints) { 	//pętla sumująca grupy liczb między elementami wspólnymi ciągów
+            
+            while (v1[i] != k) { sum1 += v1[i]; i++; } //sumowanie grupy z 1 ciągu do wspólnego elementu
+            
+            while (v2[j] != k) { sum2 += v2[j]; j++; } //sumowanie grupy z 2 ciągu do wspólnego elementu
+            
+            largest += bigger(sum1, sum2) + k; 	//dodanie większej sumy liczb między wspólnymi elementami ciągów
+            i++; j++;
+            sum1 = sum2 = 0;
+        }
+        
+        while (i < n1) { sum1 += v1[i]; i++; } //sumowanie liczb od ostatniego wspólnego elementu do końca 1 ciągu
+        
+        while (j < n2) { sum2 += v2[j]; j++; } //sumowanie liczb od ostatniego wspólnego elementu do końca 2 ciągu
+        
+        largest += bigger(sum1, sum2); 		//dodanie większej sumy liczb końcowych
+
+        std::cout << largest << std::endl;
+    }
+
+    return 0;
+}
